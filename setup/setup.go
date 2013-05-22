ee22aacf40b401e4cca704c09b73df470403a6cc
Stefan Schulz <schulz.stefan@gmail.com>
2013-05-22 21:13:23 +0200

    time added to setup

--- setup/setup.go
+++ setup/setup.go
@@ -4,15 +4,17 @@ package setup
 import (
 	"code.google.com/p/ginta"
 	"code.google.com/p/ginta/fmt/nr"
-	"code.google.com/p/ginta/fmt/quoted"
 	"code.google.com/p/ginta/fmt/plural"
+	"code.google.com/p/ginta/fmt/quoted"
+	"code.google.com/p/ginta/fmt/time"
 )
 
-func Setup(providers... ginta.LanguageProvider) {
+func Setup(providers ...ginta.LanguageProvider) {
 	nr.Install()
 	quoted.Install()
 	plural.Install()
-	
+	time.Install()
+
 	for _, p := range providers {
 		ginta.Register(p)
 	}
